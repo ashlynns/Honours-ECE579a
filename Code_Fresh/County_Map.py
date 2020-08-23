41ed79a58d30b0e60892325f848809873b256816
Ashlynn <ashlynn.steeves@iicloud.com>
2020-08-22 22:25:45 -0700

    updates

--- Code_Fresh/County_Map.py
+++ Code_Fresh/County_Map.py
@@ -9,14 +9,18 @@ County_data['State'] = df['Province_State']
 County_data['County'] = df['Admin2']
 County_data['Lat'] = df['Lat']
 County_data['Long'] = df['Long_']
+#print(list(County_data.State=='Hawaii'))
+
+County_data = County_data[County_data.State!='Hawaii']
 
 gdf = gpd.GeoDataFrame(County_data, geometry=gpd.points_from_xy(County_data.Long, County_data.Lat))
 USA = gpd.read_file('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/Maps/cb_2018_us_state_500k/cb_2018_us_state_500k.shp')
 USA = USA.drop(index = [13,27,37,38,44,45])
+USA = USA.drop(index = [42])
 
-ax = USA.plot(color='white', edgecolor='black')
-gdf.plot(ax=ax, color='red', markersize=1)
-ax.set_xlabel('Longitude')
-ax.set_ylabel('Latitude')
-plt.title('Counties Represented in Analysis')
+ax = USA.plot(color='white', edgecolor='grey')
+gdf.plot(ax=ax, color='black', markersize=2)
+ax.set_xlabel('Longitude', fontsize=24)
+ax.set_ylabel('Latitude', fontsize=24)
+plt.title('Counties Represented in Analysis', fontsize=24)
 plt.show()
---
+++ Code_Fresh/Feature_Merge.py
@@ -0,0 +1,15 @@
+import pandas as pd 
+
+AQI_Features = pd.read_csv('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/AQI_Features.csv')
+Covid_Cases_Det_Classes = pd.read_csv('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/COVID/Case_Curve_Class_D.csv')
+Covid_Cases_Spectral = pd.read_csv('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/Spectral_Clustering_Classes.csv')
+Cases_and_Deaths = pd.read_csv('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/Normalized_Cases_Deaths.csv')
+
+Features = AQI_Features
+Features['Norm_Cases'] = Cases_and_Deaths.Cases
+Features['Norm_Deaths'] = Cases_and_Deaths.Deaths
+Features['Case_Det_Class'] = Covid_Cases_Det_Classes.Case_Curve_Class 
+Features['Case_Spectral_Class'] = Covid_Cases_Spectral.SpectralClusteringK3
+
+
+Features.to_csv('/Users/Ashlynn/Desktop/Summer 2020/Honours-ECE579a/Code_Fresh/Datasets/Features.csv', index=False)
